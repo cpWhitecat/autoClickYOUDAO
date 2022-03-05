cda4c1181cd2625034654d2cc41dd93a044e3cd7
cpWhitecat <93856406+cpWhitecat@users.noreply.github.com>
2022-03-05 08:57:59 +0800

    Add files via upload

---
+++ youDaoHost.js
@@ -0,0 +1,24 @@
+
+//.sp.dictvoice.voice-js.log-js
+//overflow
+//实现网页有道自动点击，为以后埋坑
+Element.prototype._addEventListener = Element.prototype.addEventListener;
+Element.prototype.addEventListener = function () {
+  let args = [...arguments];
+  let temp = args[1];
+    args[1] = function () {
+    let args2 = [...arguments];
+      args2[0] = Object.assign({}, args2[0]);
+      args2[0].isTrusted = true;
+      return temp(...args2);
+    };
+  return this._addEventListener(...args);
+};
+
+let aALL= document.querySelector(".sp.dictvoice.voice-js.log-js");
+setInterval((aALL) => {
+     aALL.click();
+}, 2000);
+
+
+    
